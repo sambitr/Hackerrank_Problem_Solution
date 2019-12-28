57e3b57743356eb37401ff044c532952392f41d8
Sambit Rout <33625178+sambitr@users.noreply.github.com>
2019-12-28 22:16:35 +0530

    Create sockMerchant.py

---
+++ sockMerchant.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    ar.sort()
+    i = 0
+    count = 0
+    while i < n-1:
+        if ar[i] == ar[i+1]:
+            count+=1
+            i+=2
+        else:
+            i+=1
+    
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
