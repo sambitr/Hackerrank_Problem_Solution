f57bb5376ae60cdf7d6706a2b5b46cbdc596a44d
Sambit Rout <sambit_rout@hotmail.com>
2020-01-01 12:27:44 +0530

    added explanation as to how to solve this

--- saveThePrisoner.py
+++ saveThePrisoner.py
@@ -6,6 +6,12 @@ import random
 import re
 import sys
 
+"""
+In this problem, we need to determine the ID of last prisoner which can be done by moving M-1 steps further from S.
+ID of last prisoner= (M-1+S)
+Since we are moving in a circle so we need to take mod of this summation with N.
+Because the ID starts from 1, so the ID of last prisoner= (M – 1 + S – 1) % N + 1)
+"""
 # Complete the saveThePrisoner function below.
 def saveThePrisoner(n, m, s):
     value = (n+m+s-1)%n
