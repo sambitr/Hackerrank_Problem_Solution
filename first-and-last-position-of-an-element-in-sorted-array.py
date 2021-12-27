69ed970f1a8cf60081b8add8891fec1dcbcad073
Sambit Rout <sambit_rout@hotmail.com>
2021-12-27 18:58:03 +0530

    Update first-and-last-position-of-an-element-in-sorted-array.py

--- first-and-last-position-of-an-element-in-sorted-array.py
+++ first-and-last-position-of-an-element-in-sorted-array.py
@@ -1,5 +1,3 @@
-# https://www.codingninjas.com/codestudio/problem-details/first-and-last-position-of-an-element-in-sorted-array_1082549
-
 def leftindex(array, key, n):
     min, ans = 0, -1
     max = n-1
@@ -33,16 +31,13 @@ def rightindex(array, key,n):
 
     return(ans)
 
-
-array = [1,2,3,3,5]
-#[1,2,3,3,5]
-#[0,1,1,5]
-key = 4
-n = len(array)
-left_index = leftindex(array, key, n)
-right_index = rightindex(array, key, n)
-result = []
-
-result.append(left_index)
-result.append(right_index)
-print(result)
+def firstAndLastPosition(arr, n, k):
+    # Write your code here
+    result = []
+    left_index = leftindex(arr, k, n)
+    right_index = rightindex(arr, k, n)
+    result.append(left_index)
+    result.append(right_index)
+    return(result)
+    
+    pass
